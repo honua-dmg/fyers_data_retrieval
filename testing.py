150372a0436c55950fa4920b5c6a22b5dfa97501
Guru Sai Prasad Mallepalli Gajula <glavnihonua@gmail.com>
2024-07-10 21:05:52 -0400

    threading

--- testing.py
+++ testing.py
@@ -17,24 +17,21 @@ symbol.subscribe()
 time.sleep(10)
 depth.unsubscribe()
 symbol.unsubscribe()"""
-import concurrent.futures
+import threading
 from data import Depth,Symbol
 import time
-def process(type,auth_code,stonks:list,time):
+def process(class_type,auth_code,stonks,wait_time):
     access = auth_code
-    data = type(access,stonks)
+    data = class_type(access,stonks)
     data.connect()
     data.subscribe()
-    time.sleep(time)
+    time.sleep(wait_time)
     data.unsubscribe()
-
-with concurrent.futures.ProcessPoolExecutor() as exec:
-    if __name__=="__main__":
-        exec.submit(process,[Depth,
-                            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE3MjA1OTQ2MDcsImV4cCI6MTcyMDY1Nzg0NywibmJmIjoxNzIwNTk0NjA3LCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbWpqQ3ZIYmFsWGFKTmVERlBGRGFmSEJaajZncmxuY2ZhQkpEbEh2VmIxTXAya0k5a2FwbmlsOVNNLWgwV3VIQmYzb3pJcDh0cmhuVjA5VnZzNWJaTXgxM3ZMczhvb2JlSGxncVlCYk40aFk5Zi15MD0iLCJkaXNwbGF5X25hbWUiOiJNQUxMRVBBTExJIEdBSlVMQSBHVVJVIFNBSSBQUkFTQUQiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiJjY2Q4NDkwZGNkYTc2OTU0Njk3YmE4Y2JkN2YxMGI5MWNjMTU1YmI2ODA5OGRkZjUxYjk0NTg5ZiIsImZ5X2lkIjoiWU0wODkyNyIsImFwcFR5cGUiOjEwMCwicG9hX2ZsYWciOiJOIn0.QypnBnF9c5MEWCWnYB7V6HzuR5o9R0UM9SHAKozNmoY',
-                            ['NSE:SBIN-EQ','BSE:SBIN-A','NSE:TATAMOTORS-EQ'],
-                            10])
-        exec.submit(process,[Symbol,
-                            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE3MjA1OTQ1OTIsImV4cCI6MTcyMDY1NzgzMiwibmJmIjoxNzIwNTk0NTkyLCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbWpqQ2dpb0tZVUdfdkJsUlM0Y0J2UEJ0emYyQ2tlVWk1VE5EVFl4a1k4SVZINGdsNXRsVzlKWDJybW0yNFJSS3NWNmVOYmttZlhzQWNYUW1VU1VVODhwXzZPWWotX2VxU3Y0NmtzZWl1b2FpRzI2ND0iLCJkaXNwbGF5X25hbWUiOiJNQUxMRVBBTExJIEdBSlVMQSBHVVJVIFNBSSBQUkFTQUQiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiJjY2Q4NDkwZGNkYTc2OTU0Njk3YmE4Y2JkN2YxMGI5MWNjMTU1YmI2ODA5OGRkZjUxYjk0NTg5ZiIsImZ5X2lkIjoiWU0wODkyNyIsImFwcFR5cGUiOjEwMCwicG9hX2ZsYWciOiJOIn0.XkKx8VcIvFu2yPAY2WeC_vYHJ1f3KSrcnmneVbjUVv4',
-                            ['NSE:SBIN-EQ','BSE:SBIN-A','NSE:TATAMOTORS-EQ'],
-                            10])
+dep_auth = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE3MjA1OTQ2MDcsImV4cCI6MTcyMDY1Nzg0NywibmJmIjoxNzIwNTk0NjA3LCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbWpqQ3ZIYmFsWGFKTmVERlBGRGFmSEJaajZncmxuY2ZhQkpEbEh2VmIxTXAya0k5a2FwbmlsOVNNLWgwV3VIQmYzb3pJcDh0cmhuVjA5VnZzNWJaTXgxM3ZMczhvb2JlSGxncVlCYk40aFk5Zi15MD0iLCJkaXNwbGF5X25hbWUiOiJNQUxMRVBBTExJIEdBSlVMQSBHVVJVIFNBSSBQUkFTQUQiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiJjY2Q4NDkwZGNkYTc2OTU0Njk3YmE4Y2JkN2YxMGI5MWNjMTU1YmI2ODA5OGRkZjUxYjk0NTg5ZiIsImZ5X2lkIjoiWU0wODkyNyIsImFwcFR5cGUiOjEwMCwicG9hX2ZsYWciOiJOIn0.QypnBnF9c5MEWCWnYB7V6HzuR5o9R0UM9SHAKozNmoY'
+sym_auth = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE3MjA1OTQ1OTIsImV4cCI6MTcyMDY1NzgzMiwibmJmIjoxNzIwNTk0NTkyLCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbWpqQ2dpb0tZVUdfdkJsUlM0Y0J2UEJ0emYyQ2tlVWk1VE5EVFl4a1k4SVZINGdsNXRsVzlKWDJybW0yNFJSS3NWNmVOYmttZlhzQWNYUW1VU1VVODhwXzZPWWotX2VxU3Y0NmtzZWl1b2FpRzI2ND0iLCJkaXNwbGF5X25hbWUiOiJNQUxMRVBBTExJIEdBSlVMQSBHVVJVIFNBSSBQUkFTQUQiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiJjY2Q4NDkwZGNkYTc2OTU0Njk3YmE4Y2JkN2YxMGI5MWNjMTU1YmI2ODA5OGRkZjUxYjk0NTg5ZiIsImZ5X2lkIjoiWU0wODkyNyIsImFwcFR5cGUiOjEwMCwicG9hX2ZsYWciOiJOIn0.XkKx8VcIvFu2yPAY2WeC_vYHJ1f3KSrcnmneVbjUVv4'
+stonks = ['NSE:SBIN-EQ','BSE:SBIN-A','NSE:TATAMOTORS-EQ']
+wait_time = 10
+dep_thread = threading.Thread(target=process, args=(Depth, dep_auth, stonks, wait_time))
+symb_thread = threading.Thread(target=process, args=(Symbol, sym_auth, stonks, wait_time))
+dep_thread.start()
+symb_thread.start()
